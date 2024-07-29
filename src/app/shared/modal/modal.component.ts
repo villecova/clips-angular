792824749ad41efed68535a834365c798874a714
villecova <villecova@gmail.com>
2024-07-28 19:04:38 -0600

    feat(service): added

--- src/app/shared/modal/modal.component.ts
+++ src/app/shared/modal/modal.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { ModalService } from 'src/app/services/modal.service';
 
 @Component({
   selector: 'app-modal',
   templateUrl: './modal.component.html',
-  styleUrls: ['./modal.component.scss']
+  styleUrls: ['./modal.component.scss'],
+  // providers: [ ModalService ]
 })
 export class ModalComponent implements OnInit {
+  @Input() modalID = ''
 
-  constructor() { }
+  constructor(public modal: ModalService) {
+
+  }
 
   ngOnInit(): void {
+  }
+
+  closeModal() {
+    this.modal.toggleModal(this.modalID)
   }
 
 }
